3cf5393310d9f8905d00b1959cd2fd0bf03aa73c
izzie12 <izukrific@gmail.com>
2026-03-14 23:00:31 +0000

    Added netlify identity.

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" async></script>
+      </head>
+      <body>
+        {children}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if (window.netlifyIdentity) {
+                window.netlifyIdentity.on("init", user => {
+                  if (!user) {
+                    window.netlifyIdentity.on("login", () => {
+                      document.location.href = "/admin/";
+                    });
+                  }
+                });
+              }
+            `,
+          }}
+        />
+      </body>
     </html>
   );
 }
